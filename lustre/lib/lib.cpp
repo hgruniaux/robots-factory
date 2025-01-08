ba76d5894de18e969184ace38a4bba13aefd7432
Hubert Gruniaux <hubert@gruniaux.com>
2025-01-08 10:46:23 +0100

    Fix Lustre lib functions names

--- lustre/lib/lib.cpp
+++ lustre/lib/lib.cpp
@@ -4,27 +4,27 @@
 #include <cmath>
 #include <numbers>
 
-void Lib__abs_out_step(float x, Lib__abs_out *out) {
+void Lib__abs_step(float x, Lib__abs_out *out) {
     out->x = std::abs(x);
 }
 
-void Lib__sqrt_out_step(float x, Lib__sqrt_out *out) {
+void Lib__sqrt_step(float x, Lib__sqrt_out *out) {
     out->x = std::sqrt(x);
 }
 
-void Lib__min_out_step(float x, float y, Lib__min_out *out) {
+void Lib__min_step(float x, float y, Lib__min_out *out) {
     out->x = std::min(x, y);
 }
 
-void Lib__max_out_step(float x, float y, Lib__max_out *out) {
+void Lib__max_step(float x, float y, Lib__max_out *out) {
     out->x = std::max(x, y);
 }
 
-void Lib__clamp_out_step(float x, float min, float max, Lib__clamp_out *out) {
+void Lib__clamp_step(float x, float min, float max, Lib__clamp_out *out) {
     out->x = std::clamp(x, min, max);
 }
 
-void Lib__fmod_out_step(float x, float y, Lib__fmod_out *out) {
+void Lib__fmod_step(float x, float y, Lib__fmod_out *out) {
     out->x = std::fmod(x, y);
     if (out->x < 0)
         out->x += y;
@@ -38,34 +38,34 @@ static constexpr float rad2deg(float x) {
     return x * 180.0f / std::numbers::pi_v<float>;
 }
 
-void Lib__deg2rad_out_step(float x, Lib__deg2rad_out *out) {
+void Lib__deg2rad_step(float x, Lib__deg2rad_out *out) {
     out->x = deg2rad(x);
 }
 
-void Lib__rad2deg_out_step(float x, Lib__rad2deg_out *out) {
+void Lib__rad2deg_step(float x, Lib__rad2deg_out *out) {
     out->x = rad2deg(x);
 }
 
-void Lib__sin_out_step(float x, Lib__sin_out *out) {
+void Lib__sin_step(float x, Lib__sin_out *out) {
     out->x = std::sin(deg2rad(x));
 }
 
-void Lib__cos_out_step(float x, Lib__cos_out *out) {
+void Lib__cos_step(float x, Lib__cos_out *out) {
     out->x = std::cos(deg2rad(x));
 }
 
-void Lib__tan_out_step(float x, Lib__tan_out *out) {
+void Lib__tan_step(float x, Lib__tan_out *out) {
     out->x = std::tan(deg2rad(x));
 }
 
-void Lib__asin_out_step(float x, Lib__asin_out *out) {
+void Lib__asin_step(float x, Lib__asin_out *out) {
     out->x = rad2deg(std::asin(x));
 }
 
-void Lib__acos_out_step(float x, Lib__acos_out *out) {
+void Lib__acos_step(float x, Lib__acos_out *out) {
     out->x = rad2deg(std::acos(x));
 }
 
-void Lib__atan_out_step(float x, Lib__atan_out *out) {
+void Lib__atan_step(float x, Lib__atan_out *out) {
     out->x = rad2deg(std::atan(x));
 }
