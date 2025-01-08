1ecdc275c9c7d4a3148fadb12df577b213277cab
Hubert Gruniaux <hubert@gruniaux.com>
2025-01-08 10:35:07 +0100

    Add utility functions to Lustre

--- lustre/lib/lib.h
+++ lustre/lib/lib.h
@@ -3,17 +3,44 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif // __cplusplus
+#endif// __cplusplus
 
-typedef struct Lib__foo_out {
-    float x;
-    float y;
-} Lib__foo_out;
+#define DECL_FUNC0(name)               \
+    typedef struct Lib__##name##_out { \
+        float x;                       \
+    } Lib__##name##_out;               \
+    void Lib__##name##_step(float x, Lib__##name##_out *out);
 
-void Lib__foo_step(float x, int y, Lib__foo_out* out);
+#define DECL_FUNC1(name)               \
+    typedef struct Lib__##name##_out { \
+        float x;                       \
+    } Lib__##name##_out;               \
+    void Lib__##name##_step(float x, float y, Lib__##name##_out *out);
+
+#define DECL_FUNC2(name)               \
+    typedef struct Lib__##name##_out { \
+        float x;                       \
+    } Lib__##name##_out;               \
+    void Lib__##name##_step(float x, float y, float z, Lib__##name##_out *out);
+
+DECL_FUNC0(abs)
+DECL_FUNC0(sqrt)
+DECL_FUNC1(min)
+DECL_FUNC1(max)
+DECL_FUNC2(clamp)
+DECL_FUNC1(mod)
+
+DECL_FUNC0(deg2rad)
+DECL_FUNC0(rad2deg)
+DECL_FUNC0(sin)
+DECL_FUNC0(cos)
+DECL_FUNC0(tan)
+DECL_FUNC0(asin)
+DECL_FUNC0(acos)
+DECL_FUNC0(atan)
 
 #ifdef __cplusplus
 }
-#endif // __cplusplus
+#endif// __cplusplus
 
-#endif // LUSTRE_LIB_H
+#endif// LUSTRE_LIB_H
