5fda74f0705e734da9a6b99e2fa770577cf64762
Vincent <j.vinc-sites@yahoo.com>
2025-01-08 11:19:58 +0100

    Walk

--- lustre/spider2/robot_c/robot.h
+++ lustre/spider2/robot_c/robot.h
@@ -1,4 +1,4 @@
-/* --- Generated the 7/1/2025 at 18:11 --- */
+/* --- Generated the 8/1/2025 at 11:17 --- */
 /* --- heptagon compiler, version 1.05.00 (compiled mon. sep. 23 14:27:43 CET 2024) --- */
 /* --- Command line: /home/vincent/.opam/heptagon/bin/heptc -target c robot.ept --- */
 
@@ -6,23 +6,12 @@
 #define ROBOT_H
 
 #include "robot_types.h"
-typedef struct Robot__abs_out {
+#include "lib.h"
+typedef struct Robot__mux_out {
   float r;
-} Robot__abs_out;
+} Robot__mux_out;
 
-void Robot__abs_step(float x, Robot__abs_out* _out);
-
-typedef struct Robot__min_out {
-  float r;
-} Robot__min_out;
-
-void Robot__min_step(float x, float y, Robot__min_out* _out);
-
-typedef struct Robot__modulo_out {
-  float r;
-} Robot__modulo_out;
-
-void Robot__modulo_step(float x, float y, Robot__modulo_out* _out);
+void Robot__mux_step(int x, float y, float z, Robot__mux_out* _out);
 
 typedef struct Robot__setMotor_out {
   float power;
@@ -38,7 +27,19 @@ typedef struct Robot__setMotorFast_out {
 void Robot__setMotorFast_step(float current, float goal, float maxi,
                               Robot__setMotorFast_out* _out);
 
+typedef struct Robot__step_out {
+  float hipLspeed;
+  float hipRspeed;
+  float kneeLspeed;
+  float kneeRspeed;
+} Robot__step_out;
+
+void Robot__step_step(float hipLangle, float hipRangle, float kneeLangle,
+                      float kneeRangle, float gyroscope, int goLeft,
+                      Robot__step_out* _out);
+
 typedef struct Robot__robot_mem {
+  float v_30;
   Robot__st ck;
   int pnr;
 } Robot__robot_mem;
